d99764e877dee79b05acfbc4454c8a2c30783f26
Nathalia Souza <nathalia.souza@aluno.impacta.edu.br>
2026-09-13 19:37:32 -0300

    Configuração do servidor no express

---
+++ app/src/server.js
@@ -0,0 +1,16 @@
+require('dotenv').config({path:'./.env'});
+
+const express = require('express');
+const app = express();
+
+//server information
+const port = process.env.PORT
+const host = process.env.DB_HOST
+
+app.get('/', (req, res)=>{
+    res.send('Hello, World!')
+});
+
+app.listen(port, ()=>{
+    console.log(`Server running at http://${host}:${port}`)
+});
